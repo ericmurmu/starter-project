62ccdcfadc23ac752a19b5fb8072ef711ac5a1bd
ericmurmu <murmu.eric@gmail.com>
2022-10-28 09:59:54 +0600

    breakdown the file name in diffrent name

---
+++ template/main.php
@@ -0,0 +1,10 @@
+<?php include("header.html"); ?>
+<div class="col-lg-8 mx-auto p-4 py-md-5">
+    <?php include("linkheader.html"); ?>
+
+    <?php include("list.html"); ?>
+
+    <?php include("footer.html");?>
+   
+</div>
+<?php include("index.html");?>
